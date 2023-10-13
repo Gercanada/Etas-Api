61c2584a822bd6484fe7879490299a9bf0e3fcb5
Josue Rocha <josue.rocha0809@gmail.com>
2023-10-12 18:28:21 -0600

    dev: deleted eta and updated user

--- controllers/etasController.ts
+++ controllers/etasController.ts
@@ -2,7 +2,8 @@ import { Request, Response } from 'express';
 import Usuario from '../models/usersModel';
 import bcryptjs from 'bcryptjs';
 import Eta from '../models/etasModel';
-
+import db from '../db/connection';
+import  jwt  from "jsonwebtoken";
 
 export const getEtas = async (req: Request, res: Response) => {
     const etas = await Eta.findAll();
@@ -49,6 +50,7 @@ export const postEtas = async (req: Request, res: Response) => {
 
 export const putEtas = async (req: Request, res: Response) => {
  const { id } = req.params;
+
     const { body } = req;
     try {
         const usuario = await Eta.findByPk(id);
@@ -70,15 +72,37 @@ export const putEtas = async (req: Request, res: Response) => {
 export const deleteEtas = async (req: Request, res: Response) => {
     const { id } = req.params;
     const eta = await Eta.findByPk(id);
-    if (!eta) {
-        return res.status(404).json({
-            msg: 'No existe un usuario con el id ' + id
-        });
-    }
-    //await usuario.update({ status: false });.
-     await Eta.destroy({ where: {
-        id: eta.id
-    }});
-    res.json(eta);
-}
+    const usuario = await Usuario.findOne({
+        where:{id : eta?.user_id}
+    })
+    const t = await db.transaction()
+    console.log('usuario',usuario);
+    console.log('eeta',eta);
+    const {etas_num} = usuario;
+    const deleteEta= etas_num -1;
 
+    console.log('etassss num ', etas_num)
+    
+    await usuario.update({
+        etas_num :deleteEta
+    }, { transaction: t });
+    // delete questions +++++++++++++++
+//     try{
+//     if (!eta) {
+//         return res.status(404).json({
+//             msg: 'No existe un usuario con el id ' + id
+//         });
+//     }
+//     //await usuario.update({ status: false });.
+//      await Eta.destroy({ where: {
+//         id: eta.id
+//     }});
+     res.json(eta);
+// }catch(error){
+//     await t.rollback(); // Revierte la transacción en caso de error
+//     console.log(error);
+//     res.status(500).json({
+//         msg: 'Hable con el administrador'
+//     })
+// }
+}
