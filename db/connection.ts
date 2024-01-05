ab62f9108c009bf9f0ca75e3cb837d473e20a73a
ger <ger@cp.ger-server.local>
2024-01-05 13:24:10 -0600

    dockerized

--- db/connection.ts
+++ db/connection.ts
@@ -1,12 +1,32 @@
 import { Sequelize } from 'sequelize';
 
 
-const db = new Sequelize('etas', 'root', 'password', {
+
+console.log(process.env.DB_PORT??'hasnt');
+const dbPort: number = parseInt(process.env.DB_PORT || '3306');
+const dbHost: string = process.env.DB_HOST || 'localhost';
+const dbName: string = process.env.DB_NAME || 'etas';
+const dbUser: string = process.env.DB_USER || 'root';
+const dbPassword:string = process.env.DB_PASSWORD || 'password';
+
+
+
+
+/* const db = new Sequelize('etas', 'root', 'password', {
     // host: 'mysql-node',
-     dialect: 'mysql',
+    dialect: 'mysql',
     host: 'localhost',
     database: 'etas',
-   port: 3306,
+    port: 3306,
+    // logging: false,
+}); */
+
+const db = new Sequelize(dbName, dbUser, dbPassword, {
+    // host: 'mysql-node',
+    dialect: 'mysql',
+    host: dbHost,
+    database: dbName,
+    port: dbPort,
     // logging: false,
 });
 
--- dist/routes/travelToCanadaSecController.js
+++
@@ -1,2 +0,0 @@
-"use strict";
-//# sourceMappingURL=travelToCanadaSecController.js.map
