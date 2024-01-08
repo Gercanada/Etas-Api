58ac54f88b3e2f1094f63e58588af559b74df041
ger <ger@cp.ger-server.local>
2024-01-08 13:36:25 -0600

    stubs ?

---
+++ srcgen/generators/all.ts
@@ -0,0 +1,13 @@
+import { createFile } from "./generate";
+const className = process.argv[2];
+
+if (!className) {
+    console.error("Please provide a class name");
+    process.exit(1);
+}
+
+createFile("model", className);
+createFile("request", className);
+createFile("controller", className);
+createFile("route", className);
+
