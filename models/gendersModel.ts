e41e2fef7f3604f0c1fbca955641054e94bcc4cd
Josue Rocha <josue.rocha0809@gmail.com>
2024-01-08 18:28:04 -0600

    dev:update functionality in personalinfo

---
+++ models/gendersModel.ts
@@ -0,0 +1,16 @@
+import { DataTypes } from 'sequelize';
+import db from '../db/connection';
+import PersonalInfoSec from './personalInfoSecModel';
+
+const Genders = db.define('genderpicklist', {
+    valor: {
+        type: DataTypes.STRING
+    }
+},{
+    tableName: 'genderpicklist',
+    timestamps: false,
+  })
+
+PersonalInfoSec.belongsTo(Genders, { foreignKey: 'gender_id', as: 'gender' });
+
+export default Genders;
