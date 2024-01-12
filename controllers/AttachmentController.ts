44d679a97bee7a7decc717e0b2a2028bd19191a3
ger <ger@cp.ger-server.local>
2024-01-11 18:37:42 -0600

    download and delete files from s3 buchet using location path

--- controllers/AttachmentController.ts
+++ controllers/AttachmentController.ts
@@ -2,9 +2,25 @@
 
 import { Request, Response } from "express";
 import Attachment from "../models/AttachmentModel";
-import { S3Client, PutObjectCommand, ListObjectsCommand } from "@aws-sdk/client-s3";
+import { S3Client, PutObjectCommand, ListObjectsCommand, GetObjectCommand, DeleteObjectCommand } from "@aws-sdk/client-s3";
 import * as AWS from "@aws-sdk/client-s3";
 import fs from "fs";
+
+
+const accessKey: string | any = process.env.S3_ACCESS_KEY ?? '8XCSIKA2UUQ0QG75GPN1';
+const secretKey: string | any = process.env.S3_SECRET_KEY ?? 'sGyCpLYBLnT805JPYZBukpfiKnbtyGuBqHrYmAMX';
+const region: string | any = process.env.S3_REGION ?? 'us-east-1';
+const host: string | any = process.env.S3_HOST_NAME ?? 'https://sjc1.vultrobjects.com';
+
+const client = new AWS.S3({
+    credentials: {
+        accessKeyId: accessKey,
+        secretAccessKey: secretKey
+    },
+    endpoint: host, // URL del servicio compatible con S3
+    forcePathStyle: false,
+    region: region
+});
 
 
 
@@ -70,16 +86,18 @@ export const destroy = async (req: Request, res: Response) => {
 }
 
 
-
+/**
+ * Upload file to s3 bucket
+ * @param req attachment
+ * @param res success | error
+ */
 export const uploadFile = async (req: Request, res: Response) => {
-
-
     try {
         console.log('Try to up a file')
         const file = req?.file;
         const params: any = {
             Bucket: process.env.S3_BUCKET_NAME ?? 'etas-portal',
-            Key: file.originalname,
+            Key: 'files/' + file.originalname,
             Body: file.buffer,
             ContentType: file.mimetype,
         };
@@ -93,6 +111,12 @@ export const uploadFile = async (req: Request, res: Response) => {
 
 };
 
+
+/**
+ * List content of s3 selected bucket
+ * @param req null
+ * @param res bucket content
+ */
 export const listBucketContent = async (req: Request, res: Response) => {
     try {
         const params: any = {
@@ -107,17 +131,77 @@ export const listBucketContent = async (req: Request, res: Response) => {
     }
 }
 
-const accessKey: string | any = process.env.S3_ACCESS_KEY ?? '8XCSIKA2UUQ0QG75GPN1';
-const secretKey: string | any = process.env.S3_SECRET_KEY ?? 'sGyCpLYBLnT805JPYZBukpfiKnbtyGuBqHrYmAMX';
-const region: string | any = process.env.S3_REGION ?? 'us-east-1';
-const host: string | any = process.env.S3_HOST_NAME ?? 'https://sjc1.vultrobjects.com';
 
-const client = new AWS.S3({
-    credentials: {
-        accessKeyId: accessKey,
-        secretAccessKey: secretKey
-    },
-    endpoint: host, // URL del servicio compatible con S3
-    forcePathStyle: false,
-    region: region
-});
+
+export const getObject = async (req: Request, res: Response) => {
+    try {
+        const params: any = {
+            Bucket: process.env.S3_BUCKET_NAME ?? 'etas-portal',
+            Key: '2120.webp'
+        };
+        const command = new GetObjectCommand(params);
+        const data = await client.send(command);
+        /*     console.log({ data })
+            res.status(200).json(data); */
+    } catch (error) {
+        console.log({ error })
+        res.status(500).json(error);
+    }
+}
+
+
+/**
+ * Delete file from object storage using location path
+ * @param req 
+ * @query req path/to/deletefile.ext 
+ * @param res 
+ */
+export const deleteObject = async (req: Request, res: Response) => {
+    try {
+        if (req.query.path) {
+            const obj_path = req.query.path;
+            const params: any = {
+                Bucket: process.env.S3_BUCKET_NAME ?? 'etas-portal',
+                Key: obj_path
+            };
+            const command = new DeleteObjectCommand(params);
+            const result = await client.send(command);
+            res.status(200).json(result);
+
+        }
+
+    } catch (error) {
+        console.log({ error })
+        res.status(500).json(error);
+    }
+}
+
+
+export const previewObject = async (req: Request, res: Response) => {
+    try {
+
+        if (req.query.path) {
+            const obj_path = req.query.path;
+
+            console.log({obj_path})
+            const params = {
+                Bucket: process.env.S3_BUCKET_NAME ?? 'etas-portal',
+                Key: obj_path, // Aquí deberías obtener la clave (Key) del objeto de manera dinámica
+            };
+            const command = new GetObjectCommand(params);
+            const { Body, ContentType } = await client.send(command);
+            if (Body) {
+                // Establecer el tipo de contenido correcto
+                res.setHeader('Content-Type', ContentType);
+                // Body es un stream. Reenviar este stream directamente a la respuesta.
+                Body.pipe(res);
+            } else {
+                res.status(404).send('No se encontró el archivo');
+            }
+        }
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json(error);
+    }
+};
--- routes/AttachmentRoute.ts
+++ routes/AttachmentRoute.ts
@@ -4,7 +4,7 @@ import multer from "multer";
 
 import { validarCampos } from '../middlewares/validar-campos';
 import { validarJWT } from '../middlewares/validar-jwt';
-import { listBucketContent, uploadFile } from '../controllers/AttachmentController';
+import { deleteObject, downloadObject, getObject, listBucketContent, previewObject, uploadFile } from '../controllers/AttachmentController';
 // import validaRoles from '../middlewares/validar-roles';
 
 
@@ -17,8 +17,22 @@ const upload = multer(); // Guarda los archivos en la carpeta 'uploads'
 router.get('/', [
     //validarCampos
 ], listBucketContent);
-router.post('/', upload.single('file'), [
+
+
+router.get('/object', [
     //validarCampos
+], getObject);
+
+router.get('/object/preview', [
+    //validarCampos
+], previewObject);
+
+router.delete('/object', [
+    //validarCampos
+], deleteObject);
+
+
+router.post('/', upload.single('file'), [
 ], uploadFile);
 
 export default router;
