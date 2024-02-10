f71b947db7118e4240defac542e5b8354cf3fe16
heribertolord96 <heribertolord@gmail.com>
2024-02-10 17:06:23 -0600

    emmiting messages

--- models/server.ts
+++ models/server.ts
@@ -44,6 +44,8 @@ class Server {
         attachments: '/api/attachments',
     }
 
+
+    private personas;
     constructor() {
         this.app = express();
         this.port = process.env.PORT || '8000';
@@ -54,11 +56,17 @@ class Server {
         this.routes();
         // listRoutes(th);
         this.httpServer = http.createServer(this.app); // Crea un servidor http a partir de tu aplicación Express
-        this.io = new SocketIOServer(this.httpServer); // Crea un servidor Socket.IO a partir del servidor http
+        this.io = new SocketIOServer(this.httpServer, {
+            cors: {
+                origin: "*",
+                methods: ["GET", "POST"]
+            }
+        }); // Crea un servidor Socket.IO a partir del servidor http
+        this.personas = [];
     }
 
-    /*   async dbConnection() {
-  
+    // const [personas, setpPrsonas] = useState();
+    /*   async dbConnection() {//!db connection  
           try {
               await db.authenticate();
               console.log('Database online');
@@ -70,12 +78,11 @@ class Server {
       } */
 
     middlewares() {
-
         // CORS
         //this.app.use(cors());
         const corsOptions = {
-            // origin: 'http://localhost:3000',
-            origin: '*',
+            origin: 'http://localhost:3000',
+            // origin: '*',
             methods: ['GET', 'POST', 'OPTIONS', 'PUT', 'DELETE'],
             allowedHeaders: [
                 'Origin',
@@ -91,11 +98,8 @@ class Server {
         };
 
         this.app.use(cors(corsOptions));
-
-
         // Lectura del body
         this.app.use(express.json());
-
         // Carpeta pública
         this.app.use(express.static('public'));
     }
@@ -120,18 +124,114 @@ class Server {
     }
 
 
+    agregarPersona(id: number | string, nombre: string, sala: string) {
+        let persona = { id, nombre, sala };
+        this.personas.push(persona);
+        return this.personas;
+
+    }
+
+    getPersona(id) {
+        console.log({ this: this.personas })
+        let persona = this.personas.filter(persona => persona.id === id)[0];
+        return persona;
+    }
+
+    getPersonas() {
+        return this.personas;
+    }
+
+    getPersonasPorSala(sala) {
+        let personasEnSala = this.personas.filter(persona => persona.sala === sala);
+        return personasEnSala;
+    }
+
+    borrarPersona(id) {
+        let personaBorrada = this.getPersona(id);
+        this.personas = this.personas.filter(persona => persona.id != id);
+        return personaBorrada;
+    }
+
+    crearMensaje = (nombre, mensaje) => {
+        return {
+            nombre,
+            mensaje,
+            fecha: new Date().getTime()
+        };
+
+    }
+
+
+
 
     // Socket
     listen() {
         // Configura rutas y middleware de tu aplicación Express aquí
-
         // Configura Socket.IO para escuchar conexiones
-        this.io.on('connection', (socket) => {
+        /*     this.io.on('connection', (socket) => {
+                console.log('Un cliente se ha conectado a Socket.IO');
+                // Aquí puedes configurar lógica para manejar eventos de Socket.IO
+                socket.on('disconnect', () => {
+                    console.log('Un cliente se ha desconectado de Socket.IO');
+                });
+            }); */
+
+        /* interface CallbackResponse {
+            error: boolean;
+            mensaje: string;
+        } */
+
+        this.io.on('connection', (client) => {
             console.log('Un cliente se ha conectado a Socket.IO');
-            // Aquí puedes configurar lógica para manejar eventos de Socket.IO
-            socket.on('disconnect', () => {
-                console.log('Un cliente se ha desconectado de Socket.IO');
+            client.on('entrarChat', (data, callback) => {
+                console.log('entraron a sala')
+                if (!data.nombre || !data.sala) {
+                    return callback({
+                        error: true,
+                        mensaje: 'El nombre/sala es necesario'
+                    });
+                }
+                client.join(data.sala);
+                this.agregarPersona(client.id, data.nombre, data.sala);
+                client.broadcast.to(data.sala).emit('listaPersona', this.getPersonasPorSala(data.sala));
+                client.broadcast.to(data.sala).emit('crearMensaje', this.crearMensaje('Administrador', `${data.nombre} se unió`));
+                callback(this.getPersonasPorSala(data.sala));
             });
+
+            client.on('crearMensaje', (data, callback) => {
+                console.log('crearMensaje')
+                console.log({ data })
+                let persona = this.getPersona(client.id);
+                let mensaje = persona ? this.crearMensaje(persona.nombre ?? 'unamed', data.mensaje ?? "no message") : null;
+                console.log({ mensaje })
+
+                if (persona && mensaje) {
+                    client.broadcast.to(persona.sala).emit('crearMensaje', mensaje);
+                    if (typeof callback === "function") callback(mensaje);
+
+                }
+            });
+
+
+            client.on('disconnect', () => {
+                console.log('disconnect')
+                let personaBorrada = this.borrarPersona(client.id);
+                if (personaBorrada) {
+                    client.broadcast.to(personaBorrada.sala).emit('crearMensaje', this.crearMensaje('Administrador', `${personaBorrada.nombre} salió`));
+                    client.broadcast.to(personaBorrada.sala).emit('listaPersona', this.getPersonasPorSala(personaBorrada.sala));
+                }
+
+
+            });
+
+            // Mensajes privados
+            client.on('mensajePrivado', data => {
+                console.log('mensajePrivado')
+                let persona = this.getPersona(client.id);
+                client.broadcast.to(data.para).emit('mensajePrivado', this.crearMensaje(persona.nombre, data.mensaje));
+
+            });
+
         });
         // Inicia el servidor http en el puerto especificado
         this.httpServer.listen(this.port, () => {
